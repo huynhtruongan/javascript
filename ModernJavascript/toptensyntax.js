f42295ec9c04acb8cd1ac6c6123c0dd822a6dec1
Nguyen Huynh Truong An <huhuhaha.not@gmail.com>
2018-11-09 15:22:57 +0700

    add modern javascript tricks

---
+++ ModernJavascript/toptensyntax.js
@@ -0,0 +1,10 @@
+/*---| 1.Temple String |---*/
+    // The old way
+    const age = 10;
+    let message = 'i am ' + age + ' years old';
+     console.log(message); // i am 10 years old;
+     // The new way
+     message = `i am ${age} years old`;// use backsticks button
+     console.log(message);
+/*---[ End Temple String ]---*/
+/*---| 2.Exchanging data in variables  |---*/    
